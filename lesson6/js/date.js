a92ab98292e69dd625a00fb2610e15f0d41a9df4
brherman95 <brherman95@byui.edu>
2020-02-13 20:32:55 -0700

    changes

--- lesson6/js/date.js
+++ lesson6/js/date.js
@@ -1,4 +1,5 @@
 var currentdate = new Date();
 var dayofweek = new Array("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday");
 var monthsofyear = new Array("Januray", "February", "March", "April", "May", "June", "July", "August", "Septemeber", "October", "November", "December");
-document.write(dayofweek[currentdate.getDay()] + ", " + currentdate.getDate() + " " + monthsofyear[currentdate.getMonth()] + " " + currentdate.getFullYear());
+var fulldate = dayofweek[currentdate.getDay()] + ", " + currentdate.getDate() + " " + monthsofyear[currentdate.getMonth()] + " " + currentdate.getFullYear();
+document.getElementsByClassName('currentdate').innerHTML = fulldate;
--- lesson6/js/windchill.js
+++ lesson6/js/windchill.js
@@ -1,13 +1,13 @@
-const temperature = parseFloat(document.getElementById('t').textContent);
-const windspeed = parseFloat(document.getElementById('ws').textContent);
+const temperature = parseFloat(document.getElementsByClassName('t').textContent);
+const windspeed = parseFloat(document.getElementsByClassName('ws').textContent);
 
 if(temperature <= 50 && windspeed >= 3){
     const windchill = (35.74 + (0.6215*temperature) - (35.75 * Math.pow(windspeed, 0.16)) + (0.4275 * temperature * Math.pow(windspeed, 0.16))).toFixed(2); 
-    document.getElementById('wc').textContent = windchill;
+    document.getElementsByClassName('wc').textContent = windchill;
 }
 else {
     const windchill = ('N/A');
-    document.getElementById('wc').textContent = windchill;
+    document.getElementsByClassName('wc').textContent = windchill;
 }
 
 
