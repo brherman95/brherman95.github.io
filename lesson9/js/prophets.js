c2d8f04797ab39de35db1ad393f1b9bf8814bde0
brherman95 <brherman95@byui.edu>
2020-03-05 08:10:00 -0700

    changes

--- lesson9/js/prophets.js
+++ lesson9/js/prophets.js
@@ -6,24 +6,29 @@ fetch(requestURL)
   })
   .then(function (jsonObject) {
     console.table(jsonObject);  // temporary checking for valid response and data parsing
-  });
 
-  const prophets = jsonObject['prophets'];
+    const prophets = jsonObject['prophets'];
 
   for (let i = 0; i < prophets.length; i++ ) {
     let card = document.createElement('section');
     let h2 = document.createElement('h2');
-    let p = document.createElement('p');
+    let p1 = document.createElement('p');
+    let p2 = document.createElement('p');
     let image = document.createElement('img');
 
     h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;
-    p.textContent = 'Date of Birth: ' + prophets[i].birthdate + '<br>Place of Birth: ' + prophets[i].birthplace;
+    p1.textContent = 'Date of Birth: ' + prophets[i].birthdate;
+    p2.textContent = 'Place of Birth: ' + prophets[i].birthplace;
     image.setAttribute('src', prophets[i].imageurl);
 
     card.appendChild(h2);
-    card.appendChild(p);
+    card.appendChild(p1);
+    card.appendChild(p2);
     card.appendChild(image);
     
     document.querySelector('div.cards').appendChild(card);
   }
+  });
+
+  
 
