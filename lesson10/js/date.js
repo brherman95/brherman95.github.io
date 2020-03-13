c34cfc2b5ade5a82dada248d01acfb95477ee9f1
brherman95 <brherman95@byui.edu>
2020-03-13 10:01:10 -0600

    changes

---
+++ lesson10/js/date.js
@@ -0,0 +1,5 @@
+var currentdate = new Date();
+var dayofweek = new Array("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday");
+var monthsofyear = new Array("Januray", "February", "March", "April", "May", "June", "July", "August", "Septemeber", "October", "November", "December");
+var fulldate = dayofweek[currentdate.getDay()] + ", " + currentdate.getDate() + " " + monthsofyear[currentdate.getMonth()] + " " + currentdate.getFullYear();
+document.getElementById('currentdate').textContent = fulldate;
