d53bf0d0d16e57717960ab97f266ce66d62ca107
brherman95 <brherman95@byui.edu>
2020-03-14 11:52:59 -0600

    week 10 api preston

--- lesson10/js/windchill.js
+++ lesson10/js/windchill.js
@@ -1,10 +1,11 @@
-const temperature = parseFloat(document.getElementById('t').textContent);
-const windspeed = parseFloat(document.getElementById('ws').textContent);
+const temperature = parseFloat(document.getElementById('').textContent);
+const windspeed = parseFloat(document.getElementById('windspeed').textContent);
 
 if (temperature <= 50 && windspeed >= 3) {
     const windchill = (35.74 + (0.6215 * temperature) - (35.75 * Math.pow(windspeed, 0.16)) + (0.4275 * temperature * Math.pow(windspeed, 0.16))).toFixed(2);
-    document.getElementById('wc').textContent = windchill;
+    document.getElementById('windchill').textContent = windchill;
+    document.getElementById('degree').style.display = none;
 } else {
     const windchill = ('N/A');
-    document.getElementById('wc').textContent = windchill;
+    document.getElementById('windchill').textContent = windchill;
 }
