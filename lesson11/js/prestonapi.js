f7cc468bccaf3bbb7da66bacd00f3ba697d9374d
brherman95 <brherman95@byui.edu>
2020-03-20 10:35:05 -0600

    changes

--- lesson11/js/prestonapi.js
+++ lesson11/js/prestonapi.js
@@ -1,16 +1,18 @@
-const apiURL = "https://api.openweathermap.org/data/2.5/forecast?id=5604473&units=imperial&APPID=5010198a7ccffc1141920945a4c4dea2";
+const apiURL = "https://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&appid=5010198a7ccffc1141920945a4c4dea2";
+const forecastapiURL = "https://api.openweathermap.org/data/2.5/forecast?id=5604473&units=imperial&APPID=5010198a7ccffc1141920945a4c4dea2";
+
 fetch(apiURL)
   .then((response) => response.json())
 
   .then((jsObject) => {
     console.log(jsObject);
-    const temperature = parseFloat(Math.round(jsObject.list[0].main.temp));
-    const windspeed = parseFloat(Math.round(jsObject.list[0].wind.speed));
-    document.getElementById('currently').textContent = jsObject.list[0].weather[0].main;
+    const temperature = parseFloat(Math.round(jsObject.main.temp));
+    const windspeed = parseFloat(Math.round(jsObject.wind.speed));
+    document.getElementById('currently').textContent = jsObject.weather[0].main;
     document.getElementById('temp').textContent = temperature;
-    document.getElementById('high').textContent = Math.round(jsObject.list[0].main.temp_max);
+    document.getElementById('high').textContent = Math.round(jsObject.main.temp_max);
     document.getElementById('windspeed').textContent = windspeed;
-    document.getElementById('humidity').textContent = Math.round(jsObject.list[0].main.humidity);
+    document.getElementById('humidity').textContent = Math.round(jsObject.main.humidity);
 
     if (temperature <= 50 && windspeed >= 3) {
       const windchill = Math.round((35.74 + (0.6215 * temperature) - (35.75 * Math.pow(windspeed, 0.16)) + (0.4275 * temperature * Math.pow(windspeed, 0.16))).toFixed(2));
@@ -21,6 +23,12 @@ fetch(apiURL)
       document.getElementById('windchill').textContent = windchill;
       document.getElementById('degree').style.display = 'none';
     }
+
+  });
+
+  fetch(forecastapiURL)
+  .then((response) => response.json())
+  .then((jsObject) => {
 
     for (i = 0; i <= 39; i++) {
       if (jsObject.list[i].dt_txt.includes("18:00:00")) {
@@ -53,7 +61,6 @@ fetch(apiURL)
 
       }
     }
-
   });
 
   const requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
