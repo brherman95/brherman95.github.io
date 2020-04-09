9bf171458378cffc05b3f0d4ceffb02c9da42dae
brherman95 <brherman95@byui.edu>
2020-04-09 17:21:28 -0600

    changes

--- final-project/js/guides.js
+++ final-project/js/guides.js
@@ -12,8 +12,15 @@ fetch(requestURL)
     for (let i = 0; i < guides.length; i++) {
         let card = document.createElement('section');
         let article = document.createElement('article');
-        let h2 = document.createElement('h2');
+        let h3 = document.createElement('h3');
         let image = document.createElement('img');
+        let div1 = document.createElement('div');
+        let div2 = document.createElement('div');
+        let div3 = document.createElement('div');
+        let div4 = document.createElement('div');
+        let div5 = document.createElement('div');
+        let div6 = document.createElement('div');
+        let div7 = document.createElement('div');
         let p1 = document.createElement('p');
         let p2 = document.createElement('p');
         let p3 = document.createElement('p');
@@ -22,19 +29,33 @@ fetch(requestURL)
 
 
 
-        h2.textContent = guides[i].name;
+        h3.textContent = guides[i].name;
         image.setAttribute('src', guides[i].photo);
         image.setAttribute('alt', 'Portrait of our tour guide ' + guides[i].name);
-        p1.textContent = 'Certification Level: ' + guides[i].certification;
-        p2.textContent = 'Years of Experience: ' + guides[i].experience;
-        p3.textContent = 'Email Address: ' + guides[i].email;
+        div1.setAttribute('class', 'details');
+        div2.setAttribute('class', 'label');
+        div3.setAttribute('class', 'info');
+        div4.setAttribute('class', 'label');
+        div5.setAttribute('class', 'info');
+        div6.setAttribute('class', 'label');
+        div7.setAttribute('class', 'info');
+        div2.textContent = 'Certification Level: ';
+        div3.textContent = guides[i].certification;
+        div4.textContent = 'Years of Experience: ';
+        div5.textContent = guides[i].experience;
+        div6.textContent = 'Email Address: ';
+        div7.textContent = guides[i].email;
         p4.textContent = 'Biography: ' + guides[i].bio;
      
-        article.appendChild(h2);
+        article.appendChild(h3);
         article.appendChild(image);
-        article.appendChild(p1);
-        article.appendChild(p2);
-        article.appendChild(p3);
+        article.appendChild(div1);
+        div1.appendChild(div2);
+        div1.appendChild(div3);
+        div1.appendChild(div4);
+        div1.appendChild(div5);
+        div1.appendChild(div6);
+        div1.appendChild(div7);
         article.appendChild(p4);
         card.appendChild(article);
         document.querySelector('div.cards').appendChild(card);
